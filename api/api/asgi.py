1183b48ea31cc1f3d21534b44bd2c1f54f56373d
Ahmed Elghareeb <me@rancho.me>
2025-11-12 22:40:01 +0100

    refactor: restructures the django app

--- api/api/asgi.py
+++
@@ -1,9 +0,0 @@
-import os
-import django
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
-django.setup()
-
-application = get_asgi_application()
-
--- api/api/wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-import django
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
-django.setup()
-
-application = get_wsgi_application()
-
