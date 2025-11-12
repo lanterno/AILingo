f82ea2a41875ad3ddc5032a4f3c0478bdae651a3
Ahmed Elghareeb <me@rancho.me>
2025-11-12 22:40:50 +0100

    refactor: restructures the django charts app

---
+++ api/charts/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class ChartsConfig(AppConfig):
+    """Configuration for the charts app."""
+    
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "charts"
+    verbose_name = "Charts"
+
---
+++ api/charts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from charts.views import ChartViewSet
+
+router = DefaultRouter()
+router.register(r"", ChartViewSet, basename="chart")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
